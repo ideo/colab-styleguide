8be58509fe8df3c66f4d9b7cfcade1d38794f00d
Gavin McDermott <gavinmcdermott@gmail.com>
2016-07-28 12:09:18 -0700

    add basic build file

---
+++ build.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const exec = require('child_process').exec;
+
+const cssMinify =   `cat ` +
+                    `public/base.css ` +
+                    `public/colors.css ` +
+                    `public/forms.css ` +
+                    `public/grid.css ` +
+                    `public/icons.css ` +
+                    `public/navigation.css ` +
+                    `public/typography.css ` +
+                    // `| cleancss -o dist/css/colab-styleguide.min.css ` + // playing around with the api
+                    `| cleancss > dist/css/colab-styleguide.min.css `
+
+
+exec(cssMinify, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
